5f60711cea53230dd69a3c32636c3609b78e4697
Jeffrey Chan <jeffreychan@jeffreys-mbp-4.dyn.berkeley.edu>
2018-03-19 18:17:17 -0700

    Update calling of defiNETti

--- example/run_example.py
+++ example/run_example.py
@@ -6,7 +6,7 @@ import simulator_example
 
 # Train the network --  Use small num_batches for example purposes.
 prior_rates, weight_prob = simulator_example.parse_hapmap_empirical_prior(['genetic_map_GRCh37_chr1_truncated.txt'])
-defiNETti.train((198,24,2), (2,), lambda: simulator_example.simulate_data(prior_rates, weight_prob), 
+train((198,24,2), (2,), lambda: simulator_example.simulate_data(prior_rates, weight_prob), 
                 phi_net = [('conv', 5, 32),('conv', 5, 64)], 
                 h_net = [("fc",128),("fc",128),("softmax",)],
                 num_batches = 2000,
@@ -22,4 +22,4 @@ data = []
 for i in range(500):
     data_i, _ = simulator_example.simulate_data(prior_rates, weight_prob) # Typically you should feed in your real data here instead of simulated.
     data.append(data_i)
-output = defiNETti.test(data, model_path='./example_weights', threads = 5)
+output = test(data, model_path='./example_weights', threads = 5)
