ff06d3ae99633282d41255e00531a9d53b8a0370
Jeffrey Chan <jeffreychan@jeffreys-mbp-4.dyn.berkeley.edu>
2018-03-20 16:44:57 -0700

    Include setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from distutils.extension import Extension
+
+setup(name='defiNETti',
+      version='1.0.0',
+      description='A Likelihood-Free Inference Framework for Population Genetic Data using Exchangeable Neural Networks',
+      author='Jeffrey Chan, Valerio Perrone, Jeffrey P. Spence, Paul A. Jenkins, Sara Mathieson, Yun S. Song',
+      author_email='chanjed@berkeley.edu, v.Perrone@warwick.ac.uk, spence.jeffrey@berkeley.edu, p.Jenkins@warwick.ac.uk, smathie1@swarthmore.edu, yss@berkeley.edu',
+      packages=['defiNETti'],
+      install_requires=['numpy>=1.14.2','tensorflow==1.6.0'],
+      )
